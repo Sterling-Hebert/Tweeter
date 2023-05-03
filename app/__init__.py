d5055cb7b4265295cdeaf91a75ac24143370c5d7
sterling_hebert <sterling_hebert@outlook.com>
2023-05-02 19:39:12 -0500

    completed phase two part 2

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,17 @@
 # !!START
-from flask import Flask
+from flask import Flask, render_template
 from .config import Config
+from .tweets import tweets
+from random import choice
 
 app = Flask(__name__)
 
 app.config.from_object(Config)
-# !!END
+
+
+
+@app.route("/")
+def randomTweet():
+    tweet = choice(tweets)
+    print(tweet)
+    return render_template("index.html", tweet=tweet)
